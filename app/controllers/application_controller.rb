c17510e81e1b03524ce7b0a5f85c8e8f226c2381
ritika <erritika.1690@gmail.com>
2014-10-13 11:55:23 +0530

    fc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :authorize
+ # before_filter :authorize
   protect_from_forgery with: :exception
   before_filter :set_i18n_locale_from_params
 
